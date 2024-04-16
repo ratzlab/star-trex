9df3d399cec291f0b2f782d536790d670c241bae
leonievb <leonie.v.berlin@googlemail.com>
2024-04-16 08:28:02 +0200

    Two major changes: 1. spacetxer is now better in handling images with just one z-plane. 2. Primary and nuclei images are now stored in separated folders and their channel counts are independent of each other (both start with 0)

--- src/spacetxer.py
+++ src/spacetxer.py
@@ -10,9 +10,22 @@ from tifffile import TiffFile
 import xml.etree.ElementTree as ET
 import pandas as pd
 from src.path_handler import *
+import os
+
+def channel_sorter(channel, nuclei, num_channels):
+    '''
+    Re-sorts the channel number such as that the nuclei channel gets the last number. This is unfortunately necessary
+    as starfish does not allow cropping out a channel from the beginning or middle of the list in order to perform 
+    spot detection only on spot channels
+    '''
+    if channel == nuclei:
+        channel = 0
+    elif channel > nuclei:
+        channel = channel - 1
+    return channel
 
 
-def process_tiff(input_path, output_path, image_type="primary", extension=None, fov=0):
+def process_tiff(input_path, output_path, extension=None, fov=0, nuclei = 0):
     """
     Splits single round tiff images into one tiff image per round, channel and zplane. 
     Creates coordinates.csv file. Both steps are required for transformation to SpaceTx 
@@ -23,17 +36,24 @@ def process_tiff(input_path, output_path, image_type="primary", extension=None, 
           image per round OR, if fov="multi", one folder per FOV with each containg one 
           (ome).tiff per round
         - output_path: String with path to output folder
-        - image_type: Distinguishes images of sequencing rounds or additional stainings
-          such as DAPI or Nissl, can be "primary" or "nuclei", default: "primary"
         - extension: If input folder contains several file types, a specific file extension
           such as ".tiff" can be provided to select only certain files, default: None
         - fov: The given fov, can either be an number (int) or "multi". If "multi"
           is given, a folder with several input folders (one per FOV) needs to be provided,
           default: 0
+        - nuclei: Number of the nuclei channel (channel numbering starts with 0). Indicate None
+          if there is no nuclei channel. Default: 0.
     """
     #Check the paths and convert to Path object
     input_path = path_maker(input_path)
     output_path = path_maker(output_path)
+
+    #Create the separate spot and nuclei output folders
+    if not os.path.exists(os.path.join(output_path, "primary")):
+        os.makedirs(os.path.join(output_path, "primary"))
+    if nuclei != None:
+        if not os.path.exists(os.path.join(output_path, "nuclei")):
+            os.makedirs(os.path.join(output_path, "nuclei"))
 
     #Get the input image files
     if fov == "multi":
@@ -45,7 +65,9 @@ def process_tiff(input_path, output_path, image_type="primary", extension=None, 
         path_list = [get_files_in_path(input_path, extension)]
 
     #Initate a coordinate pandas table
-    coord_df = pd.DataFrame(columns=["fov", "round", "ch","zplane","xc_min","yc_min","zc_min","xc_max","yc_max","zc_max"])
+    coord_primary = pd.DataFrame(columns=["fov", "round", "ch","zplane","xc_min","yc_min","zc_min","xc_max","yc_max","zc_max"])
+    if nuclei != None:
+        coord_nuclei = pd.DataFrame(columns=["fov", "round", "ch","zplane","xc_min","yc_min","zc_min","xc_max","yc_max","zc_max"])
 
     #Loop through the FOVs
     for fov_num in range(len(path_list)):
@@ -57,10 +79,12 @@ def process_tiff(input_path, output_path, image_type="primary", extension=None, 
             
             #Read the metadata as XML tree
             with TiffFile(path_list[fov_num][round]) as tiff:
-                print(tiff)
                 root = ET.fromstring(tiff.ome_metadata)
             url = root.tag.strip("OME")
-            
+
+            #The below line is useful for reading and understanding the xml metadata
+            #print(ET.tostring(root, encoding='utf8').decode('utf8'))
+
             # Extract the number of channels and zplanes
             try:
                 num_zplanes, num_channels, pixelx, pixely = tiff_stack.shape
@@ -78,28 +102,46 @@ def process_tiff(input_path, output_path, image_type="primary", extension=None, 
             for channel in range(num_channels):
                 for zplane in range(num_zplanes):
                     # Extract a specific plane
-                    single_plane = tiff_stack[zplane,channel, :, :]
+                    if num_zplanes > 1:
+                        single_plane = tiff_stack[zplane,channel, :, :]
+                    else:
+                        single_plane = tiff_stack[channel, :, :]
 
                     #Extract and build physical coordinates table
-                    for plane in root.iter(url + 'Plane'):
-                        if plane.attrib["TheZ"] == str(zplane) and plane.attrib["TheC"] == str(channel):
-                            minX = float(plane.attrib["PositionX"])
-                            minY = float(plane.attrib["PositionY"])
-                            minZ = float(plane.attrib["PositionZ"])
+                    for tiffdata in root.iter(url + 'TiffData'):
+                        if tiffdata.attrib["FirstZ"] == str(zplane) and tiffdata.attrib["FirstC"] == str(channel):
+                            #This code is not yet for multiple fovs in one image!
+                            minX = 0.0
+                            #minX = float(tiffdata.attrib["PositionX"])
+                            minY = 0.0
+                            #minY = float(tiffdata.attrib["PositionY"])
+                            minZ = zplane * sizeZ
+                            #minZ = float(tiffdata.attrib["PositionZ"])
                             maxX = minX + float(sizeX)
                             maxY = minY + float(sizeY)
                             maxZ = minZ + sizeZ
-                            coord_df.loc[len(coord_df)] = [fov_num, round, channel, zplane, minX, minY, minZ, maxX, maxY, maxZ]
-
+                            if channel == nuclei:
+                                image_type = "nuclei"
+                                channel = channel_sorter(channel, nuclei, num_channels)
+                                coord_nuclei.loc[len(coord_nuclei)] = [fov_num, round, channel, zplane, minX, minY, minZ, maxX, maxY, maxZ]
+                            else:
+                                image_type = "primary"
+                                channel = channel_sorter(channel, nuclei, num_channels)
+                                coord_primary.loc[len(coord_primary)] = [fov_num, round, channel, zplane, minX, minY, minZ, maxX, maxY, maxZ]
+                            
                     # Save the extracted image
-                    full_output_path = f"{output_path}/{image_type}-f{fov_num}-r{round}-c{channel}-z{zplane}.tiff"
+                    full_output_path = f"{output_path}/{image_type}/{image_type}-f{fov_num}-r{round}-c{channel}-z{zplane}.tiff"
                     tifffile.imsave(full_output_path, single_plane)
                     print(f"Saved: {full_output_path}")
     
     #Save coordinates as csv file
     for col in ["fov", "round", "ch", "zplane"]:
-        coord_df[col] = coord_df[col].astype(int)                
-    coord_df.to_csv(output_path / "coordinates.csv", index=False)
+        coord_primary[col] = coord_primary[col].astype(int) 
+        if nuclei != None:
+            coord_nuclei[col] = coord_nuclei[col].astype(int)                      
+    coord_primary.to_csv(output_path / "primary/coordinates.csv", index=False)
+    if nuclei != None:
+        coord_nuclei.to_csv(output_path / "nuclei/coordinates.csv", index=False)
 
 
 
@@ -154,7 +196,6 @@ def process_forISS(input_path, output_path, image_type="primary", extension=None
             
             # Extract the number of channels and zplanes
             num_zplanes, num_channels, pixelx, pixely = tiff_stack.shape
-
             #Extract the physical size of the planes
             for pixel in root.iter(url + "Pixels"):
                 sizeX = float(pixel.attrib["PhysicalSizeX"]) * int(pixelx)
